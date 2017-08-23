cb1e7a777dce2303ac6141e20992b5bdb06a454a
叶佳栩 <yejiaxu@sf-express.com>
2017-08-23 12:30:01 +0800

    update

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() toggle = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
   openSidebar() {
-
+    this.toggle.emit();
   }
 
 }
