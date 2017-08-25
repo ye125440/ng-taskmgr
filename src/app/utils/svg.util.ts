1da4900c24777ad576dd1a3af9ed2ee8d22e542e
叶佳栩 <yejiaxu@sf-express.com>
2017-08-25 14:40:03 +0800

    sidebar added

--- src/app/utils/svg.util.ts
+++ src/app/utils/svg.util.ts
@@ -2,5 +2,12 @@ import { MdIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
 export const loadSvgResource = (ir: MdIconRegistry, ds: DomSanitizer) => {
-  ir.addSvgIcon('list', ds.bypassSecurityTrustResourceUrl('assets/Viewlist.svg'))
+  const imgDir = 'assets/img';
+  const sidebarDir = `${imgDir}/sidebar`;
+  ir.addSvgIcon('day', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`));
+  ir.addSvgIcon('week', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`));
+  ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`));
+  ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`));
+  ir.addSvgIcon('projects', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`));
+
 };
